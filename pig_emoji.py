5731ccbd3337519ea955c553b83813b66e23fe2f
Dream-Coder <341949798@qq.com>
2026-03-30 21:59:41 +0800

    Add files via upload

---
+++ pig_emoji.py
@@ -0,0 +1,19 @@
+ # -*- coding: utf-8 -*-
+import time
+pig="🐷"
+track=["_" for _ in range(40)]
+position=0
+print("准备开始! 小猪要跑到终点啦! ")
+time.sleep(1)
+while position<len(track)-1:
+	print("\n"*20)
+	position+=1
+	for i in range(len(track)):
+		if i==position:
+			print(pig,end="")
+		else:
+			print("_",end="")
+	print()
+	time.sleep(0.5)
+print("\n恭喜!小猪跑到终点了! ")
+input("按 Enter 键关闭窗口...")
